78630ef3ae22d04901ca12275f352862f3683aaf
zzengcs <mszzeng@gmail.com>
2022-05-31 15:17:34 -0700

    update grpc

--- services/common/constants.go
+++ services/common/constants.go
@@ -1,7 +1,8 @@
 package constants
 
 const (
-	TEMPORAL_PORT            = "7233"
-	TEMPORAL_ENV             = "TEMPORAL"
-	COMPUTE_GRPC_SERVER_PORT = 40051
+	TEMPORAL_PORT             = "7233"
+	TEMPORAL_ENV              = "TEMPORAL"
+	COMPUTE_GRPC_SERVER_PORT  = 40051
+	TOPOLOGY_GRPC_SERVER_PORT = 40055
 )
