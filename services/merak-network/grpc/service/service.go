bbcee06ba56ef595ecb2560d08cdd5a3dfb9f329
Yan Mo <andymo96@gmail.com>
2022-06-13 13:06:45 -0700

    Now gRPC is working, working on parsing and strature the data.

--- services/merak-network/grpc/service/service.go
+++ services/merak-network/grpc/service/service.go
@@ -1,20 +1,19 @@
 package service
 
+//package main
+
 import (
 	"context"
 	"flag"
-	"log"
-
 	pb "github.com/futurewei-cloud/merak/api/proto/v1/merak"
 	constants "github.com/futurewei-cloud/merak/services/common"
-	virtualnetwork "github.com/futurewei-cloud/merak/services/merak-network/workflows/virtual_network"
-	"go.temporal.io/sdk/client"
+	"github.com/futurewei-cloud/merak/services/merak-network/activities"
+	"log"
 )
 
 var (
-	Port            = flag.Int("port", constants.NETWORK_GRPC_SERVER_PORT, "The server port")
-	workflowOptions client.StartWorkflowOptions
-	returnMessage   = pb.ReturnMessage{
+	Port          = flag.Int("port", constants.NETWORK_GRPC_SERVER_PORT, "The server port")
+	returnMessage = pb.ReturnMessage{
 		ReturnCode:    pb.ReturnCode_FAILED,
 		ReturnMessage: "Unintialized",
 	}
@@ -24,23 +23,14 @@ type Server struct {
 	pb.UnimplementedMerakNetworkServiceServer
 }
 
-var TemporalClient client.Client
-
-func (s *Server) NetworkHandler(ctx context.Context, in *pb.InternalNetConfigInfo) (*pb.ReturnMessage, error) {
+func (s *Server) InternalNetConfigConfigurationHandler(ctx context.Context, in *pb.InternalNetConfigInfo) (*pb.ReturnMessage, error) {
 	log.Printf("Received on NetworkHandler %s", in)
 	// Parse input
 
 	switch op := in.OperationType; op {
 	case pb.OperationType_INFO:
-		workflowOptions = client.StartWorkflowOptions{
-			ID:        "InfoWorkflow",
-			TaskQueue: "Info",
-		}
+		log.Println("Info")
 	case pb.OperationType_CREATE:
-		workflowOptions = client.StartWorkflowOptions{
-			ID:        "CreateWorkflow",
-			TaskQueue: "Create",
-		}
 		for _, services := range in.Configuration.Services {
 			log.Println(services)
 		}
@@ -49,6 +39,8 @@ func (s *Server) NetworkHandler(ctx context.Context, in *pb.InternalNetConfigInf
 		}
 		for _, network := range in.Configuration.Networks {
 			log.Println(network)
+			ctx := context.TODO()
+			activities.VnetCreate(ctx, network)
 		}
 		for _, storage := range in.Configuration.Storages {
 			log.Println(storage)
@@ -56,29 +48,19 @@ func (s *Server) NetworkHandler(ctx context.Context, in *pb.InternalNetConfigInf
 		for _, extraInfo := range in.Configuration.ExtraInfo {
 			log.Println(extraInfo)
 		}
+		returnMessage.ReturnCode = pb.ReturnCode_OK
+		returnMessage.ReturnMessage = "NetworkHandler: OperationType_CREATE"
+		return &returnMessage, nil
 	case pb.OperationType_UPDATE:
-		workflowOptions = client.StartWorkflowOptions{
-			ID:        "UpdateWorkflow",
-			TaskQueue: "Update",
-		}
+		log.Println("Update")
 	case pb.OperationType_DELETE:
-		workflowOptions = client.StartWorkflowOptions{
-			ID:        "DeleteWorkflow",
-			TaskQueue: "Delete",
-		}
+		log.Println("Delete")
 	default:
 		log.Println("Unknown Operation")
 		returnMessage.ReturnCode = pb.ReturnCode_FAILED
 		returnMessage.ReturnMessage = "NetworkHandler: Unknown Operation"
 		return &returnMessage, nil
 	}
-
-	we, err := TemporalClient.ExecuteWorkflow(context.Background(), workflowOptions, virtualnetwork.Create, "Temporal")
-	if err != nil {
-		log.Fatalln("Unable to execute workflow", err)
-	}
-
-	log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID())
 
 	return &returnMessage, nil
 }
