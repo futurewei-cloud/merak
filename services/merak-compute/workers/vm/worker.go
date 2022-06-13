0f9b6edee8ea63749d254da83f9acae6e05190a3
Phu Tran <22720475+phudtran@users.noreply.github.com>
2022-06-13 12:30:25 -0700

    VM workflow

--- services/merak-compute/workers/vm/worker.go
+++ services/merak-compute/workers/vm/worker.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"strconv"
@@ -8,10 +9,14 @@ import (
 
 	constants "github.com/futurewei-cloud/merak/services/common"
 	"github.com/futurewei-cloud/merak/services/merak-compute/activities"
+	"github.com/futurewei-cloud/merak/services/merak-compute/activities/common"
 	"github.com/futurewei-cloud/merak/services/merak-compute/workflows/vm"
+	"github.com/go-redis/redis/v9"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 )
+
+var ctx = context.Background()
 
 func main() {
 
@@ -33,11 +38,30 @@ func main() {
 	}
 	defer c.Close()
 
-	w := worker.New(c, "VmCreate", worker.Options{})
+	//Connect to Redis
+	var redisAddress strings.Builder
+	redisAddress.WriteString(constants.COMPUTE_REDIS_ADDRESS)
+	redisAddress.WriteString(":")
+	redisAddress.WriteString(strconv.Itoa(constants.COMPUTE_REDIS_PORT))
 
+	common.RedisClient = *redis.NewClient(&redis.Options{
+		Addr:     redisAddress.String(),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err = common.RedisClient.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		log.Fatalln("ERROR: Unable to create Redis client", err)
+	}
+	defer common.RedisClient.Close()
+	log.Println("Connected to DB!")
+
+	w := worker.New(c, constants.COMPUTE_TASK_QUEUE, worker.Options{})
 	w.RegisterWorkflow(vm.Create)
 	w.RegisterActivity(activities.VmCreate)
-
+	log.Println("Registered VM Workflows and activities.")
+	log.Println("Starting VM Worker.")
 	err = w.Run(worker.InterruptCh())
 	if err != nil {
 		log.Fatalln("Unable to start worker", err)
---
+++ services/merak-compute/workflows/vm/vm.go
@@ -0,0 +1,25 @@
+package vm
+
+import (
+	"time"
+
+	"github.com/futurewei-cloud/merak/services/merak-compute/activities"
+	"go.temporal.io/sdk/workflow"
+)
+
+func Create(ctx workflow.Context) (string, error) {
+	ao := workflow.ActivityOptions{
+		StartToCloseTimeout: 1 * time.Second,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+	logger := workflow.GetLogger(ctx)
+	var result string
+	logger.Info("VmCreate starting workflow.")
+	err := workflow.ExecuteActivity(ctx, activities.VmCreate).Get(ctx, &result)
+	if err != nil {
+		logger.Error("VmCreate failed!", "Error", err)
+		return "FAILED", err
+	}
+	logger.Info("VmCreate workflow completed.", "result", result)
+	return result, nil
+}
