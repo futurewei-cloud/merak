891b27a3ef3de207a1ddc6d8a08401a8eb6e98a7
Yan Mo <andymo96@gmail.com>
2022-06-16 14:15:03 -0700

    vpc, subnet, router linked

--- services/merak-network/activities/vnet_create_activity.go
+++ services/merak-network/activities/vnet_create_activity.go
@@ -7,9 +7,14 @@ import (
 	"log"
 )
 
+type attach_router struct {
+	router_id string
+	subnet    string
+}
+
 type router struct {
-	router_id  string
-	subnet_ids []string
+	router_id string
+	vpc_id    string
 }
 
 type subnet struct {
@@ -45,6 +50,7 @@ func VnetCreate(ctx context.Context, network *merak.InternalNetworkInfo) (string
 	// Subnet
 	subnets := []subnet{}
 	subnetCount := 0
+	subnet_id_map := map[string]string{}
 	for i := 0; i < int(network.NumberOfVpcs); i++ {
 		vpc_id := vpcs[i].vpc_id
 		for j := 0; j < int(network.NumberOfSubnetPerVpc); j++ {
@@ -55,22 +61,30 @@ func VnetCreate(ctx context.Context, network *merak.InternalNetworkInfo) (string
 				subnet_cider: network.SubnetCiders[subnetCount],
 				vpc_id:       vpc_id,
 			}
+			subnet_id_map[network.SubnetCiders[subnetCount]] = subnet_id
 			subnets = append(subnets, current_subnet)
 			subnetCount++
 		}
 	}
 
 	// Router
-	//router_subnet_map := map[string]string{}
-	//
-	//for _, r := range network.Routers {
-	//	router_id := uuid.New().String()
-	//	for _, subnet := range r.Subnets {
-	//		current_router := router{
-	//			router_id:  router_id,
-	//			subnet_ids: subnet,
-	//		}
-	//	}
-	//}
+	routers := []router{}
+	attach_subnet := []attach_router{}
+	for _, r := range network.Routers {
+		router_id := uuid.New().String()
+		current_router := router{
+			router_id: router_id,
+			vpc_id:    vpcs[0].vpc_id, //default only one vpc right now
+		}
+		routers = append(routers, current_router)
+		for _, subnet := range r.Subnets {
+			current_subnet_attach := attach_router{
+				router_id: router_id,
+				subnet:    subnet_id_map[subnet],
+			}
+			attach_subnet = append(attach_subnet, current_subnet_attach)
+		}
+	}
+
 	return "VnetCreate", nil
 }
